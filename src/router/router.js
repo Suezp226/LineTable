2b220b3eee3ad1c53cd4c5ad7d61f45d06750393
Suezp <suzhipeng@zjmx.com>
2021-09-24 09:26:01 +0800

    路由权限拦截

--- src/router/router.js
+++ src/router/router.js
@@ -73,4 +73,18 @@ const router = createRouter({
     routes
 })
 
+// 进入路由前的拦截  可以做权限判断
+// router.beforeEach((to, from, next) => {
+//     document.title = `${to.meta.title} | vue-manage-system`;
+//     const role = localStorage.getItem("ms_username");
+//     if (!role && to.path !== "/login") {
+//       next("/login");
+//     } else if (to.meta.permission) {
+//       // 如果是管理员权限则可进入，这里只是简单的模拟管理员权限而已
+//       role === "admin" ? next() : next("/403");
+//     } else {
+//       next();
+//     }
+//   });
+
 export default router;
